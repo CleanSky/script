0b15dc3d9e53b2966693418a48c6fcb0352bf0ba
邹炳松 <zoubingsong@163.com>
2013-04-08 16:23:03 +0800

    Update wifi.bat

--- Windows Script/wifi.bat
+++ Windows Script/wifi.bat
@@ -1,3 +1,4 @@
 ﻿#mode：是否启用虚拟WiFi网卡，改为disallow则为禁用。ssid：无线网名称，用英文。key：无线网密码，八个以上字符。
 netsh wlan set hostednetwork mode=allow ssid=CleanSky key=12345678
 netsh wlan start hostednetwork
+netsh wlan stop hostednetwork
